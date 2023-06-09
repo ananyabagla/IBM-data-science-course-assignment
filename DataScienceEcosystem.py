de8f48c0e7a38ad7ca3ca3f0cdf36b252cef1b49
ananyabagla <129501826+ananyabagla@users.noreply.github.com>
2023-06-09 22:19:53 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# ## Introduction
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# <b> Objectives: </b>
+# <ul>
+#     <li>List Popular Data Science Languages</li>
+#     <li>List Data Science Libraries</li>
+#     <li>Data Science Tools</li>
+#     <li>Arithmetic Expression Examples in Python</li>
+#     </ul>
+#     
+#     
+
+# ## Data Science Languages
+# Some of the popular languages that Data Scientists use are:
+# <ol type = '1'>
+# <li>Python</li>
+# <li>R</li>
+# <li>Julia</li>
+# </ol>
+
+# ## Data Science Libraries
+# Some of the commonly used libraries used by Data Scientists include:
+# <OL>
+# <li>numpy</li>
+# <li>pandas</li>
+# <li>matplotlib</li>
+# <li>seaborn</li>
+# </OL>    
+
+# ## Data Science tools
+# <table border = 2>
+#     <tr>
+#         <th>Data Science Tools</th>
+#     </tr>
+#     <tr>
+#         <td>RStudio</td>
+#         </tr>
+#     <tr>
+#         <td>Apache Spark</td>
+#         </tr>
+#     <tr>
+#         <td>Jupyter Notebook</td>
+#         </tr>
+#     </table>
+
+# ## Arithmetic Expression Example
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[ ]:
+
+
+(3*4)+5
+#This a simple arithmetic expression to mutiply then add integers   
+
+
+# In[2]:
+
+
+200/60
+# This will convert 200 minutes to hours by diving by 60
+
+
+# ## Author
+# Mr. Ananya Vikram Bagla
